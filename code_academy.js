302d46c5b7f5c04d5ce4f4b1b538bcd544e23828
unknown <mszastak@iteria.pl>
2020-05-08 12:37:19 +0200

    interpolate

--- code_academy.js
+++ code_academy.js
@@ -4,3 +4,6 @@ changeMe = false;
 console.log(changeMe);
 const entree = 'Enchiladas';
 console.log(entree);
+var myName = "Gosia";
+var myCity = "Krakow";
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
